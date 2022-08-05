2b31c67ce042f7a4c686a36e4581428936d5df96
SunRise-11 <cleanpinesunrise03@gmail.com>
2022-08-05 14:26:02 +0100

    init application

---
+++ backend/src/main/java/com/bt/guestbook/GuestbookApplication.java
@@ -0,0 +1,13 @@
+package com.bt.guestbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuestbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuestbookApplication.class, args);
+	}
+
+}
