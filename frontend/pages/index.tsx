e02e0e9a43fcff58fde084bff0c257dafb64a83e
SunRise-11 <cleanpinesunrise03@gmail.com>
2022-08-06 16:10:01 +0100

    update: added fetch req to api for posts update: added post form

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,7 +1,29 @@
 import type { NextPage } from "next";
+import useSWR from "swr";
+import Post, { PostType } from "./components/Post";
+import PostForm from "./components/PostForm";
+
+const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
 const Home: NextPage = () => {
-  return <div className="text-blue-600">test tailwind</div>;
+  const { data: posts, error } = useSWR<PostType[]>(
+    "http://localhost:8080/api/post",
+    fetcher
+  );
+
+  if (error) return <div>failed to load</div>;
+  if (!posts) return <div>loading...</div>;
+
+  return (
+    <div className="space-y-6">
+      <PostForm />
+
+      {posts !== undefined &&
+        posts.map((post) => {
+          return <Post key={post.id} post={post} />;
+        })}
+    </div>
+  );
 };
 
 export default Home;
