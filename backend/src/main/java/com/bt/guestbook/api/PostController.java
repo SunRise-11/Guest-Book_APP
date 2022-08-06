4b7034f2e553ac736292fe24db2dde5ba6aa22ab
SunRise-11 <cleanpinesunrise03@gmail.com>
2022-08-06 11:46:35 +0100

    update: added full CRUD functionality to posts

--- backend/src/main/java/com/bt/guestbook/api/PostController.java
+++ backend/src/main/java/com/bt/guestbook/api/PostController.java
@@ -2,19 +2,17 @@ package com.bt.guestbook.api;
 
 import com.bt.guestbook.model.Post;
 import com.bt.guestbook.service.PostService;
-import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
 import java.util.List;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @RestController
 @RequestMapping("/api")
@@ -24,19 +22,38 @@ public class PostController {
 
     private final PostService postService;
 
-    @GetMapping("/post")
-    public ResponseEntity<List<Post>>getPosts() {
+    @GetMapping(path = "/post", produces = APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<Post>> getPosts() {
         return ResponseEntity.ok().body(postService.getPosts());
     }
 
-    @PostMapping("/post")
-    public ResponseEntity<Post>createPost(@RequestBody Post post) {
-        URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/post").toUriString());
-        return ResponseEntity.created(uri).body(postService.savePost(post, SecurityContextHolder.getContext().getAuthentication().getPrincipal().toString()));
+    @GetMapping(path = "/post/pending", produces = APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<Post>> getPendingPosts() {
+        return ResponseEntity.ok().body(postService.getPendingPosts());
     }
 
-    @GetMapping("/post/approve/{id}")
-    public ResponseEntity<Post>approvedPost(@PathVariable long id) {
+    @PostMapping(path = "/post", consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
+    public ResponseEntity<Post> createPost(@RequestBody Post post) {
+        String username = SecurityContextHolder.getContext().getAuthentication().getPrincipal().toString();
+        log.info("user {} is creating a post", username);
+        URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/post").toUriString());
+        return ResponseEntity.created(uri).body(postService.savePost(post, username));
+    }
+
+    @DeleteMapping(path = "/post/{id}")
+    public ResponseEntity<?> deletePost(@PathVariable long id) {
+        postService.removePostById(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping(path = "/post", consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
+    public ResponseEntity<Post> updatePost(@RequestBody Post post) {
+        Post storedPost = postService.updatePost(post);
+        return ResponseEntity.ok().body(storedPost);
+    }
+
+    @GetMapping(path = "/post/approve/{id}")
+    public ResponseEntity<Post> approvedPost(@PathVariable long id) {
         return ResponseEntity.ok().body(postService.approvedPost(id));
     }
 }
