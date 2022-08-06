393682ea48445c40c16e7236515a0db7c5dd7c38
SunRise-11 <cleanpinesunrise03@gmail.com>
2022-08-06 10:12:57 +0100

    update: added new permissions to api endpoints

--- backend/src/main/java/com/bt/guestbook/security/SecurityConfig.java
+++ backend/src/main/java/com/bt/guestbook/security/SecurityConfig.java
@@ -32,15 +32,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         AuthenticationFilter authenticationFilter = new AuthenticationFilter(authenticationManagerBean());
-        authenticationFilter.setFilterProcessesUrl("/api/login");
+        authenticationFilter.setFilterProcessesUrl("/api/auth/login");
 
         http.cors().and().csrf().disable();
         http.sessionManagement().sessionCreationPolicy(STATELESS);
 
         http.authorizeRequests().antMatchers(GET, "/api/post").permitAll();
-        http.authorizeRequests().antMatchers("/api/login").permitAll();
+        http.authorizeRequests().antMatchers("/api/auth/**").permitAll();
 
         http.authorizeRequests().antMatchers( "/api/post/**").hasAnyAuthority("ADMIN");
+        http.authorizeRequests().antMatchers( "/api/post/approve/**").hasAnyAuthority("ADMIN");
         http.authorizeRequests().antMatchers(POST, "/api/post").hasAnyAuthority("USER");
         http.authorizeRequests().antMatchers(DELETE, "/api/post").hasAnyAuthority("USER");
         http.authorizeRequests().anyRequest().authenticated();
