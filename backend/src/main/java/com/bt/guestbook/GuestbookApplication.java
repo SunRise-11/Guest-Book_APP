8b11086f4c82d349840cd0a066fc3c1cae73a554
SunRise-11 <cleanpinesunrise03@gmail.com>
2022-08-06 16:05:16 +0100

    update: added cors override

--- backend/src/main/java/com/bt/guestbook/GuestbookApplication.java
+++ backend/src/main/java/com/bt/guestbook/GuestbookApplication.java
@@ -10,6 +10,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.ArrayList;
 
@@ -20,6 +22,15 @@ public class GuestbookApplication {
         SpringApplication.run(GuestbookApplication.class, args);
     }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**").allowedOrigins("http://localhost:3000");
+            }
+        };
+    }
     @Bean
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
