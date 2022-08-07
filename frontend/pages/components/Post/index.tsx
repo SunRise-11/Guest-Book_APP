154b96a375c27238ba6fa37db2744f7b07f6d645
SunRise-11 <cleanpinesunrise03@gmail.com>
2022-08-07 18:48:57 +0100

    update: added time ago

--- frontend/pages/components/Post/index.tsx
+++ frontend/pages/components/Post/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import TimeAgo from "../TimeAgo";
 
 export type PostType = {
   id: number;
@@ -14,10 +15,10 @@ export type PostType = {
 
 const Post: React.FC<{ post: PostType }> = ({ post }) => {
   return post.type === "text" ? (
-    <div className="py-3 px-6 border space-y-3">
-      <div className="flex justify-between">
+    <div className="py-6 px-6 border rounded-lg space-y-3 bg-white shadow-md">
+      <div className="flex justify-between text-sm">
         <p className="text-blue-600">{post.user.username}</p>
-        <p>{Date.now().toString()}</p>
+        <TimeAgo date={new Date(Date.now() - 10000)} />
       </div>
       <p className="text-lg">{post.data}</p>
     </div>
