daf28dcdea6649ddc7bccc832764ed2e248067b6
SunRise-11 <cleanpinesunrise03@gmail.com>
2022-08-07 19:21:24 +0100

    fix: fixed z index issue

--- frontend/pages/components/Login/index.tsx
+++ frontend/pages/components/Login/index.tsx
@@ -78,7 +78,7 @@ const Login: React.FC = () => {
         leaveFrom="transform opacity-100 scale-100"
         leaveTo="transform opacity-0 scale-95"
       >
-        <Menu.Items className="origin-top-right absolute right-0 mt-2 w-64 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 divide-y divide-gray-100 focus:outline-none">
+        <Menu.Items className="z-10 origin-top-right absolute right-0 mt-2 w-64 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 divide-y divide-gray-100 focus:outline-none">
           {user?.username && (
             <div className="px-4 py-3">
               <p className="text-sm">
