02228772011bbba4bed166587f77df44e5427e9d
SunRise-11 <cleanpinesunrise03@gmail.com>
2022-08-07 20:32:37 +0100

    added: token handler class

---
+++ frontend/pages/lib/tokenHandler.ts
@@ -0,0 +1,44 @@
+import Cookies from "js-cookie";
+import { User } from "../components/Login";
+
+class TokenService {
+  getLocalRefreshToken() {
+    const cookie = Cookies.get("user");
+    if (cookie) {
+      const user = JSON.parse(cookie) as undefined | User;
+      return user?.refreshToken;
+    }
+  }
+  getLocalAccessToken() {
+    const cookie = Cookies.get("user");
+    if (cookie) {
+      const user = JSON.parse(cookie) as undefined | User;
+      return user?.accessToken;
+    }
+  }
+  updateLocalAccessToken(token: string) {
+    const cookie = Cookies.get("user");
+    if (cookie) {
+      const user = JSON.parse(cookie) as undefined | User;
+      if (user) {
+        user.accessToken = token;
+        return Cookies.set("user", JSON.stringify(user));
+      }
+    }
+  }
+  getUser() {
+    const cookie = Cookies.get("user");
+    if (cookie) {
+      const user = JSON.parse(cookie) as undefined | User;
+      return user;
+    }
+  }
+  setUser(user: User) {
+    return Cookies.set("user", JSON.stringify(user));
+  }
+  removeUser() {
+    return Cookies.remove("user");
+  }
+}
+
+export default new TokenService();
