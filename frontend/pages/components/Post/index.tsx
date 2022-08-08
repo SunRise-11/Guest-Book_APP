8e8aff103b70e9d552a9a8cfedf215efaa152d8c
SunRise-11 <cleanpinesunrise03@gmail.com>
2022-08-08 22:42:04 +0100

    update: added admin approval, post timestamps

--- frontend/pages/components/Post/index.tsx
+++ frontend/pages/components/Post/index.tsx
@@ -1,5 +1,10 @@
+import { useAtom } from "jotai";
 import React from "react";
-import TimeAgo from "../TimeAgo";
+import { approvedPostsAtom, pendingPostsAtom } from "../..";
+import requestAPI from "../../lib/requestAPI";
+import Container from "../Container";
+import { userAtom } from "../Login";
+import { ImagePost, TextPost } from "./PostTypes";
 
 export type PostType = {
   id: number;
@@ -11,22 +16,66 @@ export type PostType = {
     admin: boolean;
   };
   approved: boolean;
+  createdAt: number;
+  updatedAt: number;
 };
 
 const Post: React.FC<{ post: PostType }> = ({ post }) => {
-  return post.type === "text" ? (
-    <div className="py-6 px-6 border rounded-lg space-y-3 bg-white shadow-md">
-      <div className="flex justify-between text-sm">
-        <p className="text-blue-600">{post.user.username}</p>
-        <TimeAgo date={new Date(Date.now() - 10000)} />
+  const [user] = useAtom(userAtom);
+  const [approvedPosts, setApprovedPosts] = useAtom(approvedPostsAtom);
+  const [pendingPosts, setPendingPosts] = useAtom(pendingPostsAtom);
+
+  const handleApprovePost = async (e: React.MouseEvent<HTMLElement>) => {
+    e.preventDefault();
+    const res = await requestAPI.get("/post/approve/" + post.id);
+    if (res.status === 200) {
+      setApprovedPosts([
+        ...approvedPosts,
+        {
+          ...post,
+          approved: true,
+        },
+      ]);
+      setPendingPosts(pendingPosts.filter((p) => p.id !== post.id));
+    }
+  };
+
+  const handleDenyPost = async (e: React.MouseEvent<HTMLElement>) => {
+    e.preventDefault();
+    const res = await requestAPI.delete("/post/", {
+      data: post.id,
+    });
+    if (res.status === 200) {
+      setPendingPosts(pendingPosts.filter((p) => p.id !== post.id));
+    }
+  };
+
+  return (
+    <Container className={`${user?.isAdmin ? "px-0 py-0 border-none" : ""}`}>
+      <div className={`${user?.isAdmin ? "px-6 py-6 border-x border-t" : ""}`}>
+        {post.type === "text" ? (
+          <TextPost post={post} />
+        ) : (
+          <ImagePost post={post} />
+        )}
       </div>
-      <p className="text-lg">{post.data}</p>
-    </div>
-  ) : (
-    <div>
-      <p>{post.user.username}</p>
-      <p>{post.data}</p>
-    </div>
+      {user?.isAdmin && !post.approved && (
+        <div className="font-semibold text-white border-x border-b border-stone-300 rounded-b-lg overflow-hidden">
+          <button
+            className="w-1/2 bg-red-500 hover:bg-red-600 py-2"
+            onClick={(e) => handleDenyPost(e)}
+          >
+            Deny
+          </button>
+          <button
+            className="w-1/2 bg-green-500 hover:bg-green-600 py-2"
+            onClick={(e) => handleApprovePost(e)}
+          >
+            Approve
+          </button>
+        </div>
+      )}
+    </Container>
   );
 };
 
