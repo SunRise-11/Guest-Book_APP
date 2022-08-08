a5473a764a0566237962c9b2b828a780ffadc8b5
SunRise-11 <cleanpinesunrise03@gmail.com>
2022-08-08 22:43:43 +0100

    added: standard api response class

---
+++ backend/src/main/java/com/bt/guestbook/api/ApiResponse.java
@@ -0,0 +1,17 @@
+package com.bt.guestbook.api;
+
+public class ApiResponse {
+    private String message;
+
+    public ApiResponse(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
