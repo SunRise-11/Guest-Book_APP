ec9a477e9b7bcde79cc50a6225db45034d484e01
SunRise-11 <cleanpinesunrise03@gmail.com>
2022-08-08 22:41:21 +0100

    update: intergrated new post creation modal

--- frontend/pages/components/NewPost/index.tsx
+++ frontend/pages/components/NewPost/index.tsx
@@ -1,9 +1,9 @@
-import { PhotographIcon } from "@heroicons/react/solid";
 import React, { useState } from "react";
 import { User } from "../Login";
-import TextInput from "./TextInput";
+import NewPostModal from "./NewPostModal";
 
 const NewPost: React.FC<{ user: User }> = ({ user }) => {
+  const [open, setOpen] = useState(false);
   return (
     <div className="bg-white rounded-lg shadow-md w-full py-3 px-6 flex items-start space-x-3 ">
       <div className="col-span-1 select-none">
@@ -13,14 +13,14 @@ const NewPost: React.FC<{ user: User }> = ({ user }) => {
           </span>
         </span>
       </div>
-      <TextInput />
-      <div className="col-span-1 h-12 flex items-center">
-        <button className="inline-flex items-center justify-center h-10 w-10 rounded-full bg-stone-100 hover:bg-stone-200">
-          <span className="text-lg font-medium leading-none text-stone-600 uppercase">
-            <PhotographIcon className="w-6 h-6" />
-          </span>
-        </button>
-      </div>
+
+      <button
+        onClick={() => setOpen(true)}
+        className="w-full h-12 flex items-center justify-center rounded-md bg-stone-100 hover:bg-stone-200 text-lg text-stone-700 font-semibold select-none cursor-pointer"
+      >
+        Create Post
+      </button>
+      <NewPostModal open={open} setOpen={setOpen} />
     </div>
   );
 };
